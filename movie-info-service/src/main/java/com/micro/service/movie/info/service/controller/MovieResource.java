d25d7cadfd78842973cbc1abe600eebd495ce78f
shubhamkokul <shubhamkokul@gmail.com>
2020-05-04 12:34:07 -0400

    application properties

--- movie-info-service/src/main/java/com/micro/service/movie/info/service/controller/MovieResource.java
+++ movie-info-service/src/main/java/com/micro/service/movie/info/service/controller/MovieResource.java
@@ -1,18 +1,30 @@
 package com.micro.service.movie.info.service.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.micro.service.movie.info.service.model.Movie;
+import com.micro.service.movie.info.service.model.MovieSummary;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieResource {
+	
+	@Value("${api.key}")
+    private String apiKey;
+	
+	@Autowired
+    private RestTemplate restTemplate;
 
 	@RequestMapping("/{movieId}")
 	public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
-		return new Movie(movieId, "Transformer", "Great Movie! by Micheal Bay");
+		MovieSummary movieSummary = restTemplate.getForObject(
+				"https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey, MovieSummary.class);
+		return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
 
 	}
 
